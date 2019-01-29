d95cc997ddb4461e35fe8bec06b192e55aa6e84a
Al_Taras <mohammedkhmh@Gmail.com>
2019-01-29 19:22:10 +0300

    Update Data_Acquisition_Code.ino

--- Data_Acquisition_Code.ino
+++ Data_Acquisition_Code.ino
@@ -1,7 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////////////
-//THIS IS A DEMO SOFTWARE JUST FOR EXPERIMENT PURPOER IN A NONCOMERTIAL ACTIVITY
-//Version: 1.0 (AUG, 2016)
-
 //Gyro - Arduino UNO R3
 //VCC  -  5V
 //GND  -  GND
